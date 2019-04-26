9377feba21c3d96da20a3bb77299614dddd02230
regulator95 <seinolf@gmail.com>
2019-04-26 10:51:34 -0400

    modified build.sh to use int date and shorthand hash

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TAG=`git log -1 --pretty=%H`
+TAG=`git log -1 --pretty=%at`-`git log -1 --pretty=%h`
 docker build ./ -t temp_drupal
 docker tag temp_drupal:latest ncidockerhub.nci.nih.gov/webteam/drupal:$TAG
 docker push ncidockerhub.nci.nih.gov/webteam/drupal:$TAG
