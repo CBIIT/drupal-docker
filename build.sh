3b3b6c43fad72dd903bf7ec8f1a9724bfdc57cb4
regulator95 <seinolf@gmail.com>
2019-04-26 11:04:29 -0400

    adding comments to build.sh ;

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 TAG=`git log -1 --pretty=%at`-`git log -1 --pretty=%h`
+
+echo "building drupal image"
 docker build ./ -t temp_drupal
+
+echo "tagging drupal image"
 docker tag temp_drupal:latest ncidockerhub.nci.nih.gov/webteam/drupal:$TAG
+docker tag temp_drupal:latest ncidockerhub.nci.nih.gov/webteam/drupal:latest
+
+echo "pushing images to repository"
 docker push ncidockerhub.nci.nih.gov/webteam/drupal:$TAG
-docker tag ncidockerhub.nci.nih.gov/webteam/drupal:$TAG ncidockerhub.nci.nih.gov/webteam/drupal:latest
 docker push ncidockerhub.nci.nih.gov/webteam/drupal:latest 
 
+echo "removing images from system"
 docker rmi temp_drupal:latest
 docker rmi ncidockerhub.nci.nih.gov/webteam/drupal:$TAG
 
