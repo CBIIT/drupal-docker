16f46a588a854e716c65c644af943ba58c15d462
regulator95 <seinolf@gmail.com>
2019-04-26 09:59:31 -0400

    adding build script to tag images based on latest commit

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+TAG=`git log -1 --pretty=%H`
+docker build ./ -t temp_drupal
+docker tag temp_drupal:latest ncidockerhub.nci.nih.gov/webteam/drupal:$TAG
+docker push ncidockerhub.nci.nih.gov/webteam/drupal:$TAG
+docker tag ncidockerhub.nci.nih.gov/webteam/drupal:$TAG ncidockerhub.nci.nih.gov/webteam/drupal:latest
+docker push ncidockerhub.nci.nih.gov/webteam/drupal:latest 
+
+docker rmi temp_drupal:latest
+docker rmi ncidockerhub.nci.nih.gov/webteam/drupal:$TAG
+
