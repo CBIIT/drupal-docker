053843acf8f5cbc9e18e25bb28c4a3d6fe5fd847
Shaun Einolf <seinolf@gmail.com>
2019-11-15 16:21:37 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ docker build --no-cache ./ -t temp_drupal
 
 echo "tagging drupal image"
 docker tag temp_drupal:latest ncidockerhub.nci.nih.gov/webteam/drupal:$TAG
+docker tag temp_drupal:latest ncidockerhub.nci.nih.gov/webteam/drupal:latest
 
 echo "pushing images to repository"
 docker push ncidockerhub.nci.nih.gov/webteam/drupal:$TAG
+docker push ncidockerhub.nci.nih.gov/webteam/drupal:latest
 
 echo "removing images from system"
 docker rmi ncidockerhub.nci.nih.gov/webteam/drupal:$TAG
