e873daa63c8c0c9034897b000899ed15e3854014
regulator95 <seinolf@gmail.com>
2021-10-21 13:51:41 -0400

    adding example with environment variables

--- resources/run.sh
+++ resources/run.sh
@@ -1,11 +1,16 @@
 # Install Site
-if [[ -z $dbname ]]; then
-    DBNAME=$(echo $RANDOM | md5sum | head -c 20; echo;)
-    drush si --db-url=mysql://root:password@192.168.0.178:3306/$DBNAME --site-name=Default --account-name=bob --account-pass=bob -y
+user="${user:-admin}"
+pass="${pass:-admin}"
 
-else
-    drush si --db-url=mysql://root:password@192.168.0.178:3306/$dbname --site-name=$sitename --account-name=bob --account-pass=bob -y
-fi
+dbname="${dbname:-$(echo $RANDOM | md5sum | head -c 20; echo;)}"
+dbhost="${dbhost:-localhost}"
+dbport="${dbport:-3306}"
+dbuser="${dbuser:-default}"
+dbpass="${dbpass:-default}"
+sitename="${sitename:-default}"
+
+drush si --db-url=mysql://$dbuser:$dbpass@$dbhost:$dbport/$dbname --site-name=$sitename --account-name=$user --account-pass=$pass
+    
 chown -R apache:apache /opt/drupal/web/sites/default
 #Start Apache
 exec httpd -DFOREGROUND
--- tempbuild.sh
+++ tempbuild.sh
@@ -4,6 +4,6 @@ docker rm alpine
 docker rmi alpine
 
 docker build --no-cache  ./ -t alpine
-docker run --name alpine -p 8081:80 alpine  
+docker run --name alpine -p 8081:80 -e sitename=webteam -e dbhost=192.168.0.178 -e dbuser=root -e dbpass=password -e dbname=webteam -e user=admin -e pass=1234 alpine
 #docker run -it --entrypoint bash -d --name alpine -p 8081:80 alpine
 
