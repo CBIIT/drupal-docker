25989c2cb88ea780048162161a4749e2e7720317
regulator95 <seinolf@gmail.com>
2021-10-21 10:57:54 -0400

    modifying tempbuild

--- tempbuild.sh
+++ tempbuild.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 docker kill alpine
 docker rm alpine
+docker rmi alpine
+
 docker build --no-cache  ./ -t alpine
 docker run -d --name alpine -p 8081:80 alpine  
 #docker run -it --entrypoint bash -d --name alpine -p 8081:80 alpine
