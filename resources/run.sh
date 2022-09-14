c5ab2e17b9f97233f48197dc97a3913a98c0263a
regulator95 <einolfs@nih.gov>
2022-09-14 09:28:08 -0400

    adding touched file to skip drupal install on site start if site already exists

--- resources/run.sh
+++ resources/run.sh
@@ -8,9 +8,13 @@ dbport="${dbport:-3306}"
 dbuser="${dbuser:-default}"
 dbpass="${dbpass:-default}"
 sitename="${sitename:-default}"
-
-drush si --db-url=mysql://$dbuser:$dbpass@$dbhost:$dbport/$dbname --site-name=$sitename --account-name=$user --account-pass=$pass
-    
-chown -R apache:apache /opt/drupal
+FILE=/opt/drupal/installed
+if [ -f "$FILE" ]; then
+    echo "Drupal Site Already Installed"
+else
+    drush si --db-url=mysql://$dbuser:$dbpass@$dbhost:$dbport/$dbname --site-name=$sitename --account-name=$user --account-pass=$pass
+    touch /opt/drupal/installed
+    chown -R apache:apache /opt/drupal
+fi
 #Start Apache
 exec httpd -DFOREGROUND
