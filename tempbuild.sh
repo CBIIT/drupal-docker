13e46ae05271d9eca442c18c6bb28343a806dc8b
Your Name <you@example.com>
2022-10-28 11:49:48 -0400

    removing tempbuild

--- tempbuild.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-docker kill alpine
-docker rm alpine
-docker rmi alpine
-
-docker build --no-cache  ./ -t alpine
-docker run --name alpine -p 8081:80 -e sitename=webteam -e dbhost=192.168.0.178 -e dbuser=root -e dbpass=password -e dbname=webteam -e user=admin -e pass=1234 alpine
-#docker run -it --entrypoint bash -d --name alpine -p 8081:80 alpine
-
